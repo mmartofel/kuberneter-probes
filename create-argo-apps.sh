6cd0b38680bbea86c2a059015e9d9d0a1878a4a6
mmartofel <mmartofe@redhat.com>
2022-04-01 15:22:05 +0200

    initial migroservices delivery with Argo CD

---
+++ create-argo-apps.sh
@@ -0,0 +1,11 @@
+oc delete AppProject no-kubernetes-probes -n openshift-gitops
+
+oc delete application store -n openshift-gitops
+
+oc adm policy add-cluster-role-to-user cluster-admin -z default -n probes
+oc adm policy add-cluster-role-to-user cluster-admin -z builder -n probes
+oc adm policy add-cluster-role-to-user cluster-admin -z deployer -n probes
+oc adm policy add-cluster-role-to-user cluster-admin -z pipeline -n probes
+
+oc create -f ./deployment/argo/no-kubernetes-probes.yaml -n openshift-gitops
+oc create -f ./deployment/argo/store.yaml -n openshift-gitops
