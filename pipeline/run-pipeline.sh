4bffa53b01aa747f5690498d9606a83dee0ff392
mmartofel <mmartofe@redhat.com>
2022-04-12 12:18:38 +0200

    add skopeo pipeline

--- pipeline/run-pipeline.sh
+++ pipeline/run-pipeline.sh
@@ -1,5 +1,5 @@
-tkn pipeline start quarkus-api-build-pipeline \
+tkn pipeline start skopeo-copy-to-quay \
   --namespace=project01 \
   --serviceaccount=build-bot \
-  --workspace name=default,claimName=pipeline-pvc \
+  --workspace name=shared-volume,claimName=pipeline-pvc \
   --showlog
