9b127ae44ded1950f371e8f5436dacc3cf10ca45
mmartofel <mmartofe@redhat.com>
2024-03-20 11:55:46 +0100

    modify argo apps install script

--- create-argo-apps.sh
+++ create-argo-apps.sh
@@ -1,10 +1,12 @@
 oc new-project probes
+oc new-project crud-application
 
 oc delete AppProject no-kubernetes-probes -n openshift-gitops
 
 oc delete application store  -n openshift-gitops
 oc delete application probes -n openshift-gitops
 oc delete application frontend -n openshift-gitops
+oc delete application crud-application -n openshift-gitops
 
 oc adm policy add-cluster-role-to-user cluster-admin -z default -n probes
 oc adm policy add-cluster-role-to-user cluster-admin -z builder -n probes
@@ -16,3 +18,4 @@ oc create -f ./deployment/argo/no-kubernetes-probes.yaml -n openshift-gitops
 oc create -f ./deployment/argo/store.yaml -n openshift-gitops
 oc create -f ./deployment/argo/probes.yaml -n openshift-gitops
 oc create -f ./deployment/argo/frontend.yaml -n openshift-gitops
+oc create -f ./deployment/argo/crud-application.yaml -n openshift-gitops
