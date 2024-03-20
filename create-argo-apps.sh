f259077a521b3111ed7206e014f483e092836fb4
mmartofel <mmartofe@redhat.com>
2024-03-20 12:11:47 +0100

    fix grants for argo at crud-application

--- create-argo-apps.sh
+++ create-argo-apps.sh
@@ -14,6 +14,12 @@ oc adm policy add-cluster-role-to-user cluster-admin -z deployer -n probes
 oc adm policy add-cluster-role-to-user cluster-admin -z pipeline -n probes
 oc adm policy add-role-to-user admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n probes
 
+oc adm policy add-cluster-role-to-user cluster-admin -z default -n crud-application
+oc adm policy add-cluster-role-to-user cluster-admin -z builder -n crud-application
+oc adm policy add-cluster-role-to-user cluster-admin -z deployer -n crud-application
+oc adm policy add-cluster-role-to-user cluster-admin -z pipeline -n crud-application
+oc adm policy add-role-to-user admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n crud-application
+
 oc create -f ./deployment/argo/no-kubernetes-probes.yaml -n openshift-gitops
 oc create -f ./deployment/argo/store.yaml -n openshift-gitops
 oc create -f ./deployment/argo/probes.yaml -n openshift-gitops
